7752e4a8a40773cf8d5c4f657802486ca5b73887
Mohammadreza Rezaie <mrrezaie@outlook.com>
2024-10-27 00:31:11 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,6 +12,10 @@ from .moco import *
 from . import report
 from .version import __version__
 
+# add exe and dll files to system PATH
 cwd = os.path.dirname(__file__)
+os.environ['PATH'] = cwd + os.pathsep + os.environ['PATH']
+
+# realize the path to geometries
 geometry_path = os.path.join(cwd, 'Geometry')
 ModelVisualizer.addDirToGeometrySearchPaths(geometry_path)
